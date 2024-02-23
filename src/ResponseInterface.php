b8f636d6d91e78dcdf81b991e5148a37ec0c611b
Norman Huth <norman@huth.it>
2024-02-23 12:37:07 +0100

    wip

---
+++ src/ResponseInterface.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace NormanHuth\Response;
+
+interface ResponseInterface
+{
+    /**
+     * Get the body of the response.
+     *
+     * @return string
+     */
+    public function body(): string;
+
+    /**
+     * Determine if the request was successful.
+     */
+    public function successful(): bool;
+
+    /**
+     * Determine if the response indicates a client or server error occurred.
+     */
+    public function failed(): bool;
+
+    /**
+     * Determine if the response indicates a client error occurred.
+     */
+    public function clientError(): bool;
+
+    /**
+     * Determine if the response indicates a server error occurred.
+     */
+    public function serverError(): bool;
+
+    /**
+     * Execute the given callback if there was a server or client error.
+     */
+    public function onError(callable $callback): self;
+
+    /**
+     * Throw an exception if a server or client error occurred.
+     */
+    public function throw(): self;
+
+    /**
+     * Throw an exception if a server or client error occurred and the given condition evaluates to true.
+     */
+    public function throwIf($condition): self;
+
+    /**
+     * Throw an exception if the response status code is a 4xx level code.
+     */
+    public function throwIfClientError(): self;
+
+    /**
+     * Throw an exception if the response status code is a 5xx level code.
+     */
+    public function throwIfServerError(): self;
+
+
+    /**
+     * Get the body of the response.
+     */
+    public function __toString(): string;
+}
